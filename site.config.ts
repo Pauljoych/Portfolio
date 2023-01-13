c396435df6f1740610a24aa107a2e5fb6254ac4f
Pauljoych <luthfiquart@gmail.com>
2023-01-13 12:49:19 +0300

    init

--- site.config.ts
+++ site.config.ts
@@ -2,8 +2,8 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  // https://pauljoych.notion.site/My-Resume-05639f50e0b24a4983af97c20df45fb3
-  rootNotionPageId: '05639f50e0b24a4983af97c20df45fb3',
+  // https://cedar-owl-587.notion.site/My-Resume-b58563176ca24d8f8955a48021e63c88
+  rootNotionPageId: 'b58563176ca24d8f8955a48021e63c88',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
